d9f2c71ae7658ce959c5fa442b92d3511442bbd7
Alexandre Kahn <alexandrekahn139@gmail.com>
2017-07-26 07:55:05 +0200

    Added print-status

--- Project/Controlsystems/NMBS.rkt
+++ Project/Controlsystems/NMBS.rkt
@@ -11,9 +11,29 @@
 
 (provide make-nmbs)
 
-  (define (make-nmbs)
+(define (make-nmbs)
+
+  (define (print-status)
+    (hash-for-each (rwm-dt rwm)
+                   (lambda (detection-track)
+                     (display "Detection track: ")
+                     (display (detection-track 'get-id))
+                     (display ": Is free ")
+                     (display (detection-track 'free))))
+    (for-each
+     (lambda (track)
+       (display "Track: ")
+       (display (track 'get-node1))
+       (display " ")
+       (display (track 'get-node2))
+       (display ": Is free ")
+       (display (track 'free?))
+       (newline))
+     (rwm-ts)))
+
 
   (define (dispatch msg)
     (cond
+      ((eq? msg 'print-status) print-status)
       (else (error "Unkown Message"))))
   dispatch)
