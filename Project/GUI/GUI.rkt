7203b12b492d6714c19517636c00cd7d5ec1b3ac
Alexandre Kahn <alexandrekahn139@gmail.com>
2017-07-27 22:59:23 +0200

    Finaly starting to get a bit the hang of the GUI

--- Project/GUI/GUI.rkt
+++ Project/GUI/GUI.rkt
@@ -1,35 +1,58 @@
 #lang racket/gui
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;              GUI ADT                 ;;
 ;; Copyright 2017 Alexandre Kahn 2BA CW ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require "../ADT/RailwayModel.rkt")
+(require racket/gui/base)
+; Load all the bitmaps
+ (define locomotive  (read-bitmap "loco.bmp"))
+; Make a frame by instantiating the frame% class
+(define train-frame (new frame%
+                         [label "Racket Trains"]
+                         [style '(no-resize-border)]
+                         [width 800]
+                         [height 600]))
+ 
+; Make a static text message in the frame
+(define msg (new message% [parent train-frame]
+                 [label "No events so far..."]))
+ 
+; Make a button in the frame
+(define draw-panel (new horizontal-panel% [parent train-frame]
+                               [min-height 760] [stretchable-height #f]))
 
-;(define (make-gui title infra nmbs)
-(define rwm (load-rwm "../be_simple.txt"))
-(define title "Racket Trains")
-(define frame (new frame% [label title] [width 600] [height 600]))
-;(define background (read-bitmap ))
-(define btn-panel (new horizontal-panel% [parent frame]
+(define btn-panel (new horizontal-panel% [parent train-frame]
                                [min-height 40] [stretchable-height #f]))
-(new canvas% [parent frame]
-     [paint-callback
-      (lambda (canvas dc)
-        (send dc set-scale 3 3)
-        (send dc set-text-foreground "green")
-        (send dc draw-text "Don't Panic!" 0 0))])
-(define buttons   (list (new button% [parent btn-panel]
-                                     [label "Drive"]
-                                     [callback (λ (button event)
-                                                 (display "lol"))])
-                                (new button% [parent btn-panel]
-                                     [label "Change switch"]
-                                     [callback (λ (button event)
-                                                 (display "pap-switch"))])
-                                ))
-(send frame show #t)
+
+(define combo-field (new combo-field%
+                         (label "Train")
+                         (parent btn-panel)
+                         (choices (list "Field" "Train"))
+                         (init-value "Field")))
 
 
-;)
+(new button% [parent btn-panel]
+     [label "Drive"]
+     ; Callback procedure for a button click:
+     [callback (lambda (button event)
+                 (send msg set-label "Button click"))]) ; Hier de geselecteerde trein laten rijden
+
+
+
+; Derive a new canvas (a drawing window) class to handle events
+(define my-canvas%
+  (class canvas% ; The base class is canvas%
+    ; Define overriding method to handle mouse events
+    (define/override (on-event event)
+      (send msg set-label "Canvas mouse"))
+    ; Define overriding method to handle keyboard events
+    (define/override (on-char event)
+      (send msg set-label "Canvas keyboard"))
+    ; Call the superclass init, passing on all init args
+    (super-new)))
+ 
+; Make a canvas that handles events in the frame
+(new my-canvas% [parent draw-panel])
+
+(send train-frame show #t)
