0bf0fb29026f9b5a20c81296fd3240b0d2888bb7
Alexandre Kahn <alexandrekahn139@gmail.com>
2017-08-02 01:09:18 +0200

    Scheduling now 100% correct, driving not totally

--- Project/ADT/RwmToGraph.rkt
+++ Project/ADT/RwmToGraph.rkt
@@ -42,9 +42,13 @@
   ;; For the calculation of a path, it shouldn't matter if the edge is a track,
   ;; detection track or switch. Each edge should be between to nodes
 
+  (define (add-track! node1 node2)
+    (add-edge! node-graph node1 node2))
+
+  (define (delete-track! node1 node2)
+    (delete-edge! node-graph node1 node2))
+
   (define (build-graph)
-    (define (add-track! node1 node2)
-      (add-edge! node-graph node1 node2))
     (link-nodes-to-graph)
     (hash-for-each
      (rwm-dt railwaymodel)
@@ -73,6 +77,31 @@
      (rwm-ts railwaymodel))
     )
 
+  (define (fix-switches path block1 block2)
+    (display "Attempt to switch")
+    (define correct-path path)
+    (define (fix-switch rest-of-path)
+      ; Delete the nB and nC, recalculate the shortest path and readd the deleted edge
+      (displayln "fixing")
+      (define node1 (car rest-of-path))
+      (set! node1 (hash-ref node-dict node1))
+      (define node2 (cadr rest-of-path))
+      (set! node2 (hash-ref node-dict node2))
+      (delete-track! node1 node2)
+      (displayln "deleted")
+      (set! correct-path (calculate-path block1 block2))
+      (displayln correct-path)
+      (add-track! node1 node2))
+    (define (check-loop rest-of-path) ; Only if the track could be a switch a test is needed
+      (when (<= 3 (length rest-of-path))
+        (displayln "In the when")
+        (if (eqv? (find-railwaypiece (car rest-of-path) (cadr rest-of-path)) (find-railwaypiece (cadr rest-of-path) (caddr rest-of-path)))
+            (fix-switch rest-of-path)
+            (check-loop (cdr rest-of-path)))))
+    (check-loop path)
+    (display "correct-path ")(displayln correct-path)
+    correct-path)
+
   (define (calculate-path block-1 block-2)
     (define (list-from-mcons mlist)
       (define (iterloop m-l l)
@@ -87,21 +116,24 @@
     (define start-vertex (hash-ref node-dict start-node))
     (define stop-vertex (hash-ref node-dict stop-node))
     (define schedule (reverse (map real-node (list-from-mcons (shortest-path node-graph start-vertex stop-vertex)))))
+    (displayln schedule)
     (set! schedule (make-usable-path schedule start-block stop-block))
+    (displayln schedule)
+    (set! schedule (fix-switches schedule block-1 block-2))
+    (displayln schedule)
     schedule)
 
-  (define (make-usable-path path from-block to-block)
-    (define nA (from-block 'get-node1))
-    (define nB (from-block 'get-node2))
-    (define nC (to-block 'get-node1))
-    (define nD (to-block 'get-node2))
-    (when (and (not (= (length path) 1)) (not (eq? (cadr path) nB)))
-      (set! path (list (car path) nB (cdr path))))
+  (define (make-usable-path path begin end)
+    (display "usablle ") (displayln path)
+    (define node-B   (begin 'get-node2))
+    (define node-D   (end 'get-node2))
+    (when (or (and (not (= (length path) 1)) (not (eq? (cadr path) node-B)))
+              (not (eq? (car path) node-B)))
+      (set! path (cons node-B path)))
     (set! path (reverse path))
-    (when (not (eq? (cadr path) nD))
-      (set! path (list (car path) nD (cdr path))))
-    (set! path (reverse (flatten path)))
-    path)
+    (when (not (eq? (cadr path) node-D))
+      (set! path (cons node-D path)))
+    (reverse path))
   ; Calculates the path, changes it to a list en finally, returns the list with the number-of-nodes
 
   (build-graph)
--- Project/ADT/Train.rkt
+++ Project/ADT/Train.rkt
@@ -13,6 +13,7 @@
 (define (make-train id)
   (define max-speed 20)
   (define schedule '())
+  (define direction 1)
 	(define next-detection-track '())
 
   (define (set-schedule! schedule-list)
@@ -22,16 +23,21 @@
 	(define (set-next-detection-track! dt-id)
 		(set! next-detection-track dt-id))
 
+  (define (set-direction! dir)
+    (set! direction dir))
+
   (define (dispatch msg)
     (cond
       ; getters
       ((eq? msg 'get-id) id)
+      ((eq? msg 'get-direction) direction)
       ((eq? msg 'get-max-speed) max-speed)
       ((eq? msg 'get-schedule) schedule)
 			((eq? msg 'get-next-dt) next-detection-track)
       ; Setters
       ((eq? msg 'set-schedule!) set-schedule!)
 			((eq? msg 'set-next-dt!) set-next-detection-track!)
+      ((eq? msg 'set-direction!) set-direction!)
       (else
        (error "Message not understood" msg))))
   dispatch)
